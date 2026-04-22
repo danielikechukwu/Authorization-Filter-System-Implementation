bc17a2a68872e4c40817bdbd2d4dbd36e467d15a
Daniel Okwukwe Ikechukwu <ikechukwudaniel080@gmail.com>
2026-04-22 06:07:50 +0100

    Feat(model): implement a user model

---
+++ AuthorizationFilterSystemImplementation/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace AuthorizationFilterSystemImplementation.Models;
+
+public class User
+{
+    //Unique Id of the user
+    public int Id { get; set; }
+    
+    //Name of the user
+    public string Name { get; set; } = null!;
+    
+    // Username of the user
+    public string Email { get; set; } = string.Empty;
+    
+    // Password for demo only (in real apps, store hashed passwords)
+    public string Password { get; set; } = string.Empty;
+    
+    // Roles assigned to the user, comma-separated
+    public string Roles { get; set; } = string.Empty;
+}
